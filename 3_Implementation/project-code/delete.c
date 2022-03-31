1371ece3a3c103beb21518667018148df73d0a7a
GirishSce <87637095+GirishSce@users.noreply.github.com>
2022-03-31 14:43:31 +0530

    Add files via upload

---
+++ 3_Implementation/project-code/delete.c
@@ -0,0 +1,52 @@
+#include<stdio.h>
+#include<conio.h>
+#include<ctype.h>
+#include<windows.h>
+#include<stdlib.h>
+struct subscriber
+{
+char phonenumber[20];
+char name[50];
+float amount;
+}s;
+void deleterecords()
+{
+	FILE *f,*t;
+	int i=1;
+	char phonenumber[20];
+	if((t=fopen("c:/temp.ojs","w+"))==NULL)
+	exit(0);
+	if((f=fopen("c:/file.ojs","rb"))==NULL)
+	exit(0);
+	system("cls");
+	printf("Enter the phone number to be deleted from the Database");
+	fflush(stdin);
+	scanf("%[^\n]",phonenumber);
+	while(fread(&s,sizeof(s),1,f)==1)
+	{
+		if(strcmp(s.phonenumber,phonenumber)==0)
+		{       i=0;
+			continue;
+
+		}
+		else
+			fwrite(&s,sizeof(s),1,t);
+	}
+	if(i==1)
+	{       system("cls");
+		printf("Phone number \"%s\" not found",phonenumber);
+		remove("c:/file.ojs");
+		rename("c:/temp.ojs","c:/file.ojs");
+		getch();
+		fclose(f);
+		fclose(t);
+		
+	}
+	remove("c:/file.ojs");
+	rename("c:/temp.ojs","c:/file.ojs");
+	system("cls");
+	printf("The Number %s Successfully Deleted!!!!",phonenumber);
+	fclose(f);
+	fclose(t);
+	getch();
+}
